0e9ab6e88e6af7c9212ab4262daa373e0423dd55
p1kul <funikova@me.com>
2024-10-28 19:08:49 +0300

    Add files via upload

--- ls.py
+++ ls.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 
 app = FastAPI()
 
@@ -9,18 +9,21 @@ async def all_inf() -> dict:
     return users
 
 @app.post("/user/{username}/{age}")
-async def create_message(username: str, age: str) -> str:
+async def create_message(username: str = Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")
+                         , age: str = Path(ge=18, le=120, description="Enter age",example="24")) -> str:
     user_id = str(int(max(users,key=int)) + 1)
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"User {user_id} is registered"
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def refresh(user_id: str, username: str, age: str) -> str:
+async def refresh(user_id: str = Path(ge=1, le=100, description="Enter User ID", example="1")
+                  , username: str = Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")
+                  , age: str = Path(ge=18, le=120, description="Enter age",example="24")) -> str:
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"The user {user_id} is registered"
 
 @app.delete("/user/{user_id}")
-async def delete(user_id: str) -> str:
+async def delete(user_id: str = Path(ge=1, le=100, description="Enter User ID", example="1")) -> str:
     users.pop(user_id)
 
 
