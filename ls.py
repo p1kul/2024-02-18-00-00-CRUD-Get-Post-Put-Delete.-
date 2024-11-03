18ce391ad00730f2c35c9fd8688a05a8091dee99
p1kul <funikova@me.com>
2024-11-03 13:35:28 +0300

    Add files via upload

--- ls.py
+++ ls.py
@@ -9,27 +9,23 @@ async def all_inf() -> dict:
     return users
 
 @app.post("/user/{username}/{age}")
-async def create_message(username: str = Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")
-                         , age: str = Path(ge=18, le=120, description="Enter age",example="24")) -> str:
+async def post_user(username: str = Path(min_length=5, max_length=20, description="Enter username", examples="UrbanUser")
+                         , age: int = Path(ge=18, le=120, description="Enter age",examples="24")):
     user_id = str(int(max(users,key=int)) + 1)
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"User {user_id} is registered"
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def refresh(user_id: str = Path(ge=1, le=100, description="Enter User ID", example="1")
-                  , username: str = Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")
-                  , age: str = Path(ge=18, le=120, description="Enter age",example="24")) -> str:
+async def refresh_user(user_id: int = Path(ge=1, le=100, description="Enter User ID", examples="1")
+                  , username: str = Path(min_length=5, max_length=20, description="Enter username", examples="UrbanUser")
+                  , age: int = Path(ge=18, le=120, description="Enter age",examples="24")):
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"The user {user_id} is registered"
 
 @app.delete("/user/{user_id}")
-async def delete(user_id: str = Path(ge=1, le=100, description="Enter User ID", example="1")) -> str:
-    users.pop(user_id)
-
-
-
-
-
+async def delete_user(user_id: int = Path(ge=1, le=100, description="Enter User ID", examples="1")):
+    users.pop(str(user_id))
+    return f"User {user_id} has been deleted"
 
 
 
